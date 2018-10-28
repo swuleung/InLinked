417ab94f50fe926ebb00065c0295d1de9bcff9d3
Sammi Wu Leung <sammi.wuleung@stonybrook.edu>
2018-10-27 23:55:31 -0400

    feat(queries.sql): Added unique constraint to email

--- db/queries.sql
+++ db/queries.sql
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS `User` (
     Headline VARCHAR(255),
     Password VARCHAR(255) NOT NULL,
     Email CHAR(60) NOT NULL,
-    PRIMARY KEY (UserId)
+    PRIMARY KEY (UserId),
+    UNIQUE(Email)
 );
 
 -- Inherits User
