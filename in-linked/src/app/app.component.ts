fd00cde8c0a233f52f9e17a2180d0df6321c432f
Spiderpig86 <slim679975@gmail.com>
2018-11-04 20:03:37 -0500

    feat(login): Minor update to login UI, added new colors, added padding/margin constant, cleaned up css

--- in-linked/src/app/app.component.ts
+++ in-linked/src/app/app.component.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  showNavbar = false;
-  title = 'in-linked';
+  private showNavbar: boolean = false;
+  private title: string = 'in-linked';
 }
