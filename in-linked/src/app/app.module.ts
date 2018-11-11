b854436e44ce1f812a5c463623e46c5b7ee20a26
swuleung <sammiwuleung@gmail.com>
2018-11-11 13:44:03 -0500

    chore(job-applicants): Removed extraneous page

--- in-linked/src/app/app.module.ts
+++ in-linked/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { JobApplicantsComponent } from './pages/job-applicants/job-applicants.component';
 import { CandidateProfileComponent } from './pages/candidate-profile/candidate-profile.component';
 import { CreateAccountComponent } from './pages/create-account/create-account.component';
 import { DashboardComponent } from './layouts/dashboard/container/dashboard/dashboard.component';
@@ -24,7 +23,6 @@ import { UserFeedComponent } from './pages/user-feed/user-feed.component';
     LoginComponent,
     SettingsComponent,
     SearchPageComponent,
-    JobApplicantsComponent,
     CandidateProfileComponent,
     CreateAccountComponent,
     DashboardComponent,
