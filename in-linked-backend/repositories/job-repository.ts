b18a931d06bf889b24eb685b57dc69fb1cd5b4e5
spiderpig86 <slim679975@gmail.com>
2018-11-15 19:45:02 -0500

    feat(job-repository.ts): Added base crud functionality for jobs

--- in-linked-backend/repositories/job-repository.ts
+++ in-linked-backend/repositories/job-repository.ts
@@ -1,5 +1,7 @@
 import { Job } from '../models';
 import { MySql } from '../utils/lib/database';
+import { connect } from 'http2';
+import { ValidationException, NotFoundException } from '../utils/exceptions';
 
 /**
  * CRUD functionality for jobs database
@@ -18,19 +20,80 @@ export class JobRepository {
     /* CRUD */
 
     public async insert(job: Job): Promise<Job> {
+        const conn = await this.db.getConnection();
 
+        try {
+            const res = await conn.table(this.TABLE_NAME).insert({
+                JobId: job.jobId,
+                EnterpriseId: job.enterpriseId,
+                JobTitle: job.jobTitle,
+                JobDescription: job.jobDescription,
+                Salary: job.salary,
+                EmploymentType: job.employmentType,
+                ExperienceLevel: job.experienceLevel,
+                EducationLevel: job.educationLevel,
+                City: job.city,
+                Province: job.province,
+                Country: job.country
+            });
+        } catch (err) {
+            if (err.code === 'ER_DUP_ENTRY') {
+                throw new ValidationException(
+                    `Job with id ${ job.jobId } already exists`,
+                    err
+                );
+            }
+
+            throw err; // Other errors
+        }
     }
 
     public async get(id: number): Promise<Job> {
+        const conn = await this.db.getConnection();
+        const row = await conn
+            .table(this.TABLE_NAME)
+            .where({ ExperienceId: id })
+            .first();
 
+        if (!row) {
+            throw new NotFoundException(
+                `The id '${id}' does not exist in the jobs table.`
+            );
+        }
+        return this.toModel(row);
     }
 
     public async update(job: Job): Promise<Job> {
-
+        const conn = await this.db.getConnection();
+        await conn.table(this.TABLE_NAME)
+            .where({ JobId: job.jobId })
+            .update({
+                JobTitle: job.jobTitle,
+                JobDescription: job.jobDescription,
+                Salary: job.salary,
+                EmploymentType: job.employmentType,
+                ExperienceLevel: job.experienceLevel,
+                EducationLevel: job.educationLevel,
+                City: job.city,
+                Province: job.province,
+                Country: job.country
+            });
+        return job;
     }
 
     public async delete(id: number): Promise<void> {
-
+        const transaction = await this.db.getTransaction();
+        
+        try {
+            await transaction.from(this.TABLE_NAME)
+                .delete()
+                .where({ JobId: id });
+            
+            await transaction.commit();
+        } catch (err) {
+            transaction.rollback();
+            throw err;
+        }
     }
 
     public toModel(row: any): Job {
