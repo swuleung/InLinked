2066dbff2027f62161ed3f813b25b031021e8506
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2018-11-17 01:00:34 -0500

    feat(create-account.component.ts): Worked on the HTML, component, and service for creating new user

--- in-linked/src/app/services/user.service.ts
+++ in-linked/src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Candidate } from 'src/app/models/candidate';
 import { Enterprise } from '../models/enterprise';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { catchError, map } from 'rxjs/operators';
 
 const helper = new JwtHelperService();
 
@@ -14,8 +15,27 @@ export class UserService {
   private candidateData: Candidate;
   private enterpriseData: Enterprise;
   private decoded: any;
+  private created;
 
   constructor(private http: HttpClient) { }
+
+  create(username: string, password: string, email: string, acctype: string): boolean {
+    const newUser = {
+      username: username,
+      headline: '',
+      password: password,
+      email: email,
+      profilePicture: '',
+      coverPhoto: '',
+      role: 'user',
+      acctype: acctype
+    };
+    this.http.post(`${this.apiUrl}/user`, {user: newUser})
+      .pipe(
+        map(created =>  this.created = created), // Change this later, need to return true at some point
+      );
+    return false;
+  }
 
   get(data: string): void {
     this.decoded = helper.decodeToken(data);
