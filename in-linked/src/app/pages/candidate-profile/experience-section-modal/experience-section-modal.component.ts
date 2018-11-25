2d0249a5e0efe621567aaf272359389c2cbfb35f
Spiderpig86 <slim679975@gmail.com>
2018-11-25 17:42:50 -0500

    feat(experience-section-modal.component.ts): Experience modal now prepopulates with existing experience entries

--- in-linked/src/app/pages/candidate-profile/experience-section-modal/experience-section-modal.component.ts
+++ in-linked/src/app/pages/candidate-profile/experience-section-modal/experience-section-modal.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { Experience } from '../../../models/experience';
 
 @Component({
   selector: 'app-candidate-experience-section-modal',
@@ -12,6 +13,10 @@ export class ExperienceSectionModalComponent implements OnInit {
   closeResult: string;
   experienceForm: FormGroup;
   experiences: FormArray;
+  experienceIndices: number[]; // List of experience ids already loaded (helps us determine which entries to send to server instead of the whole thing)
+
+  @Input()
+  experienceList: Experience[];
 
   constructor(private modalService: NgbModal, private formBuilder: FormBuilder) {}
 
@@ -19,32 +24,57 @@ export class ExperienceSectionModalComponent implements OnInit {
     this.experienceForm = this.formBuilder.group({
       experiences : this.formBuilder.array([ this.createExperience() ])
     });
+    this.experiences = this.experienceForm.get('experiences') as FormArray;
   }
 
   open(content) {
     this.modalService.open(content, { size: 'lg' });
+    console.log(this.experienceList);
+    console.log(this.experiences);
+
+    this.reloadFormArray();
   }
 
-  createExperience(): FormGroup {
+  createExperience(position?: string, enterprise?: string, enterpriseLocation?: string, startMonth?: number, startYear?: number, endMonth?: number, endYear?: number, experienceDescription?: string): FormGroup {
     return this.formBuilder.group({
-      position: '',
-      enterprise: '',
-      enterpriseLocation: '',
-      startMonth: '',
-      startYear: '',
-      endMonth: '',
-      endYear: '',
-      experienceDescription: ''
+      position: position || '',
+      enterprise: enterprise || '',
+      enterpriseLocation: enterpriseLocation || '',
+      startMonth: startMonth || '',
+      startYear: startYear || '',
+      endMonth: endMonth || '',
+      endYear: endYear || '',
+      experienceDescription: experienceDescription || ''
     });
   }
 
-  addExperience(): void {
+  addExperience(experience?: FormGroup): void {
     this.experiences = this.experienceForm.get('experiences') as FormArray;
-    this.experiences.push(this.createExperience());
+    this.experiences.push(experience || this.createExperience());
   }
 
   removeExperience(experienceIndex): void {
     this.experiences = this.experienceForm.get('experiences') as FormArray;
     this.experiences.removeAt(experienceIndex);
+  }
+
+  reloadFormArray() {
+    for (let i = this.experiences.length - 1; i >= 0; i--) {
+      this.experiences.removeAt(i);
+    }
+
+    // Initialize form array with all these entries
+    for (const exp of this.experienceList) {
+      this.addExperience(this.createExperience(
+        exp.positionName,
+        exp.enterpriseName,
+        exp.location,
+        exp.startMonth,
+        exp.startYear,
+        exp.endMonth,
+        exp.endYear,
+        exp.description
+      ));
+    }
   }
 }
