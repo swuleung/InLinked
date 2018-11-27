d45909488e48ccc2cf9f4f7b605ba83186318df7
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2018-11-26 22:15:15 -0500

    feat(error.component.ts): Error redirect to candidate/enterprise page

--- in-linked/src/app/pages/error/error.component.ts
+++ in-linked/src/app/pages/error/error.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user/user.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-error',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./error.component.scss']
 })
 export class ErrorComponent implements OnInit {
+  private profileType = '';
+  private profileName = '';
+  private link = '';
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.loadCurrentUser(localStorage.getItem(`${environment.token_key}`)).subscribe(
+      result => {
+        if (result) {
+          this.profileType = this.userService.candidateData ? 'candidate' : 'enterprise';
+          this.profileName = this.userService.candidateData ? this.userService.candidateData.username : this.userService.enterpriseData.username;
+          if (this.profileType === '' || this.profileName === '') {
+            this.link = '/login';
+          } else {
+            this.link = `/dashboard/${this.profileType}/${this.profileName}`;
+          }
+        }
+      }
+    )
   }
 
 }
