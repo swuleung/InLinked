a184d2d54ae5e57c13b5e57d5db156841da67bca
Jeffrey Zou <jeffrey.zou@stonybrook.edu>
2018-11-26 23:07:30 -0500

    feat(create-job): Connected job creation to services

--- in-linked/src/app/pages/create-job/create-job.component.ts
+++ in-linked/src/app/pages/create-job/create-job.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { JobService } from 'src/app/services/job/job.service';
+import { UserService } from 'src/app/services/user/user.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-create-job',
@@ -6,6 +9,18 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./create-job.component.scss']
 })
 export class CreateJobComponent implements OnInit {
+    private jobTitle = '';
+    private employmentType = '';
+    private experienceLevel = '';
+    private educationLevel = '';
+    private salary = '';
+    private city = '';
+    private province = '';
+    private country = '';
+    private jobDescription = '';
+    private jobUrl = '';
+    private errorMessage = '';
+
     private educationLevels = [
         'High School',
         'Bachelors',
@@ -31,9 +46,31 @@ export class CreateJobComponent implements OnInit {
         'Volunteer',
         'Other'
     ];
-    constructor() { }
+    constructor(private router: Router, private jobService: JobService, private userService: UserService) { }
 
     ngOnInit() {
     }
 
+    onSubmit() {
+        if (!this.jobTitle) {
+            this.errorMessage = 'Job title is required';
+            return;
+        }
+        if (!this.jobDescription) {
+            this.errorMessage = 'Job description required';
+            return;
+        }
+        if (!this.jobUrl) {
+            this.errorMessage = 'Job url required';
+            return;
+        }
+        this.jobService.create(this.userService.enterpriseData.enterpriseId, this.jobTitle, this.jobDescription, this.jobUrl, new Date(), this.salary, this.employmentType, this.experienceLevel, this.educationLevel, this.city, this.province, this.country)
+            .subscribe(result => {
+                if (!result) {
+                    this.errorMessage = 'Could not create job.';
+                } else {
+                    this.router.navigate(['/dashboard/job/viewjobs']);
+                }
+            });
+    }
 }
